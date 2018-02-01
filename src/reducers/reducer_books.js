e9096c30b7b42609324c3cac883691f48bff0088
Unknown <pagejakob@gmail.com>
2018-02-01 22:01:37 +1100

    Tutorial Complete

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 300},
+        {title: 'The Dark Tower', pages: 450},
+        {title: 'Who Moved My Cheese', pages: 88}
+    ]
+}
